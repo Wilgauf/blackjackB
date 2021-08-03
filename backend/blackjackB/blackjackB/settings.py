342eda83d6062c77089f1b6a92f945b834de3239
JFranklin <franklin.jd2142@gmail.com>
2021-08-03 14:52:32 -0400

    we

--- backend/blackjackB/blackjackB/settings.py
+++ backend/blackjackB/blackjackB/settings.py
@@ -28,12 +28,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blackjack_backend',
-    # 'corsheaders',
     'rest_framework',   
 ]
 
 MIDDLEWARE = [
-    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
