8526c56cedc3853b4e305c3244af4779b4ab00a3
JFranklin <franklin.jd2142@gmail.com>
2021-08-03 16:29:18 -0400

    rearrange files

--- backend/blackjackB/blackjackB/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework_jwt.views import obtain_jwt_token
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('blackjack_backend.urls')),
-    path('token-auth/', obtain_jwt_token)
-]
--- backend/blackjackB/blackjack_backend/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BlackjackBackendConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blackjack_backend'
