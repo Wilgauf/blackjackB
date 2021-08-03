45c85fb4111b3197416922aaa56270113b561698
JFranklin <franklin.jd2142@gmail.com>
2021-08-03 13:04:03 -0400

    update CORS for heroku

--- backend/blackjackB/blackjackB/settings.py
+++ backend/blackjackB/blackjackB/settings.py
@@ -140,6 +140,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'https://localhost:3000']
+CORS_ALLOWED_ORIGINS = ['https://blackjackfrontend.herokuapp.com/', 'http://blackjackfrontend.herokuapp.com/']
 
 django_heroku.settings(locals())
